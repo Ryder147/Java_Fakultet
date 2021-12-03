83ec23bf8f901c916dc0effc777ee57603a0b6c0
Ryder147 <83040548+Ryder147@users.noreply.github.com>
2021-12-03 14:51:59 +0100

    dodano obsługę kont

--- src/Klient.java
+++ src/Klient.java
@@ -16,18 +16,31 @@ class Klient
 }
 
 
+
 class Wypozyczalnia {
     String[] ksiazki;
     String[] ksiazkiwyp;
-    Klient[] klienci=new Klient[0];
+    Klient[] klienci=new Klient[2];
     int indexwyp=0;
 
     public Wypozyczalnia(String[] samochody, String[] samochodywyp) {
         this.ksiazki = samochody;
         this.ksiazkiwyp = samochodywyp;
     }
+    Klient Logowanie(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Na które konto chcesz się zalogować(0,1... albo 60 jeśli chcesz anulować) :\n");
+        int licznik=0;
+        for (Klient a:this.klienci) {
+            System.out.println(licznik+" "+a.login);
+            licznik+=1;
+        }
+        int wybor=scan.nextInt();
+        return klienci[wybor];
 
-    void dodajksiazke(String samochod){
+    }
+
+    void dodajksiazke(String ksiazka){
         String[] proxyArray = new String[this.ksiazki.length + 1];
 
         for (int i = 0; i < proxyArray.length; i++) {
@@ -35,7 +48,7 @@ class Wypozyczalnia {
             if (i != ksiazki.length) {
                 proxyArray[i]=this.ksiazki[i];
             }else{
-                proxyArray[i] = samochod;
+                proxyArray[i] = ksiazka;
             }
         }
         this.ksiazki = proxyArray;
@@ -156,6 +169,17 @@ class Wypozyczalnia {
 
     }
 
+    public static boolean isInteger(String s){
+        try{
+            Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+
 
 
 
@@ -168,79 +192,118 @@ class Wypozyczalnia {
         Scanner scan = new Scanner(System.in);
 
         Klient k1 = new Klient("Kamil", "elo123");
+        Klient k2=new Klient("Kacper","luka");
+
+        String[] książki = {"George R.R. Martina Pieśń Lodu i Ognia", "Ernest Hemingway Stary Człowiek i Morze", "Henryk Sienkiewicz Ogniem i Mieczem", "Henryk Sienkiewicz Potop","Henryk Sienkiewicz Pan Wołodyjowski"};
+        String[] ksiazkiwyp = new String[0];
+        Wypozyczalnia w1 = new Wypozyczalnia(książki, ksiazkiwyp);
+        w1.klienci[0]=k1;
+        w1.klienci[1]=k2;
 
 
-        String[] samochody = {"Henryk Sienkiewicz Ogniem i Mieczem ", "Ernest Hemingway Stary Człowiek i Morze", "Henryk Sienkiewicz Ogniem i mieczem", "Henryk Sienkiewicz Potop","Henryk Sienkiewicz Pan Wołodyjowski"};
-        String[] samochodywyp = new String[0];
-        Wypozyczalnia w1 = new Wypozyczalnia(samochody, samochodywyp);
+        //boolean dziala = true;
+        while (true) {
+            System.out.println("Co chcesz zrobić:\n 1-zalogować się\n 2-założyć konto\n 3-zakończyć działanie programu");
+            int opcja1=scan.nextInt();
+            if(opcja1==1) {
+
+                k1=w1.Logowanie();
+                System.out.println("Podaj hasło: ");
+                scan.nextLine();
+                String haslo= scan.nextLine();
+                if(k1.haslo.equals(haslo)) {
 
 
+                    while (true) {
+                        System.out.println("Co chcesz zrobić:\n 1-Wypożyczyć książkę\n 2-Zwrócić książkę\n 3-Sprawdzić co masz wypożyczone\n 4-Wylogować się ");
+                        int opcja = scan.nextInt();
+                        if (opcja == 1) {
 
-        boolean dziala = true;
-        while (dziala) {
-            System.out.println("Co chcesz zrobić:\n 1-Wypożyczyć książkę\n 2-Zwrócić książkę\n 3-Sprawdzić co masz wypożyczone\n 4-Zakończyć działanie programu ");
-            int opcja=scan.nextInt();
-            if(opcja==1) {
+                            System.out.println("ksiazki do wypożyczenia:\n");
+                            for (int i = 0; i < w1.ksiazki.length; i++) {
+                                System.out.println(i + " " + w1.ksiazki[i]);
+                            }
+                            if (w1.ksiazkiwyp.length != 0) {
+                                System.out.println("\nKsiążki już wypożyczone :");
+                                for (int i = 0; i < w1.ksiazkiwyp.length; i++) {
+                                    if (w1.ksiazkiwyp[i] != null)
+                                        System.out.println(i + " " + w1.ksiazkiwyp[i]);
+                                }
 
-                    System.out.println("ksiazki do wypożyczenia:\n");
-                    for (int i = 0; i < w1.ksiazki.length; i++) {
-                        System.out.println(i+" "+w1.ksiazki[i]);
-                    }
-                    if (w1.ksiazkiwyp.length != 0) {
-                        System.out.println("\nKsiążki już wypożyczone :");
-                        for (int i = 0; i < w1.ksiazkiwyp.length; i++) {
-                            if (w1.ksiazkiwyp[i] != null)
-                                System.out.println(i+" "+w1.ksiazkiwyp[i]);
+                            }
+                            System.out.println("\nWybierz książkę (0,1,2,...) albo x jeśli chcesz anulować");
+                            scan.nextLine();
+                            String wybors = scan.nextLine();
+                            if (wybors.equals("x")) {
+                                continue;
+
+
+                            }else{
+                                if(isInteger(wybors)){
+                                    w1.wypozycz(k1, Integer.parseInt(wybors));
+                                }
+                                else{
+                                    System.out.println("Nie ma takiej opcji.");
+                                }
+                            }
+                        }
+                        if (opcja == 2) {
+
+                            if (k1.wypozyczony.length != 0) {
+                                System.out.println("Którą książkę chcesz zwrócić?\n ");
+
+                                for (int i = 0; i < k1.wypozyczony.length; i++) {
+                                    System.out.println(i + " " + k1.wypozyczony[i]);
+                                }
+                                System.out.println("\nWybierz którą książkę (0,1,2,...) albo x jeśli chcesz anulować ");
+                                scan.nextLine();
+                                String wybors = scan.nextLine();
+                                if (wybors.equals("x")) {
+                                    continue;
+
+                                }else{
+                                    if(isInteger(wybors)){
+                                        w1.zwroc(k1, Integer.parseInt(wybors));
+                                    }
+                                    else{
+                                        System.out.println("Nie ma takiej opcji.");
+                                    }
+                                }
+                            } else {
+                                System.out.println("\nNie masz wypożyczonych książek. ");
+                            }
+
+
+                        }
+                        if (opcja == 3) {
+                            if (k1.wypozyczony.length != 0) {
+                                System.out.println("Masz wypożyczone:\n");
+
+                                for (int i = 0; i < k1.wypozyczony.length; i++) {
+                                    System.out.println(i + " " + k1.wypozyczony[i]);
+                                }
+                            } else {
+                                System.out.println("Nie masz wypożyczonych książek.");
+                            }
                         }
 
-                   }
-                    System.out.println("\nWybierz który samochód (0,1,2,...) albo 60 jeśli chcesz anulować");
-                    scan.nextLine();
-                    int wybors = scan.nextInt();
-                    if(wybors<60){
-                        w1.wypozycz(k1, wybors);
-                    }
 
-              //  }
-            }
-            if(opcja==2){
+                        if (opcja == 4) {
 
-                if(k1.wypozyczony.length!=0) {
-                    System.out.println("Który samochód chcesz zwrócić?\n ");
+                            break;
 
-                    for (int i = 0; i < k1.wypozyczony.length; i++) {
-                        System.out.println(i+" "+k1.wypozyczony[i]);
-                    }
-                    System.out.println("\nWybierz który samochód (0,1,2,...) albo 60 jeśli chcesz anulować ");
-                    scan.nextLine();
-                    int wybors = scan.nextInt();
-                    if(wybors<60){
-                        w1.zwroc(k1,wybors);
+                        }
+
+
                     }
                 }else{
-                    System.out.println("\nNie masz wypożyczonych samochodów. ");
-                }
-
-
-            }
-            if(opcja==3){
-                if(k1.wypozyczony.length!=0){
-                    System.out.println("Masz wypożyczone:\n");
-
-                    for (int i = 0; i < k1.wypozyczony.length; i++) {
-                        System.out.println(i+" "+k1.wypozyczony[i]);
-                    }
-                }else{
-                    System.out.println("Nie masz wypożyczonego samochodów.");
+                    System.out.println("Niepoprawne hasło !");
+                    continue;
                 }
             }
-
-
-            if(opcja==4){
-                dziala = false;
+            if(opcja1==3){
+                break;
             }
-
-
         }
 
 
